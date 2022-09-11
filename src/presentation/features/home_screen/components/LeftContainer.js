3d5a7c6fb76df20f2e19a6896212bbaa11b2f42a
Md Hadiuzzaman <hadi@heavytask.com>
2022-09-11 18:26:15 +0600

    feat: design portfolio option

--- src/presentation/features/home_screen/components/LeftContainer.js
+++ src/presentation/features/home_screen/components/LeftContainer.js
@@ -1,6 +1,26 @@
 import ContainerStyle from "./LeftContainer.module.css";
 
+const url =
+  "https://yt3.ggpht.com/yti/AJo0G0mEQSBSpuCHeLyaw0BWRpxgYyGI80ZLdzj6XJLWMw=s108-c-k-c0x00ffffff-no-rj";
 const LeftContainer = (props) => {
-  return <div className={ContainerStyle.left}>Left</div>;
+  return (
+    <div className={ContainerStyle.left}>
+      <div className={ContainerStyle.center}>
+        <img src={url} alt="Avatar"></img>
+        <p></p>
+        <h2>Md Hadiuzzaman</h2>
+        <p>Software Engineer @HeavyTask LLC</p>
+        <p></p>
+
+            <button>About Me</button>
+            <button>Educations</button>
+            <button>Skills</button>
+            <button>Projects</button>
+            <button>Blog</button>
+            <button>Contacts</button>
+        
+      </div>
+    </div>
+  );
 };
 export default LeftContainer;
